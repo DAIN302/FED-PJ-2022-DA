3a946b11c8cb064cb0b6683d968501714ec6c5d1
DAIN302 <dbgnfk12@gmail.com>
2023-02-13 11:33:43 +0900

    CGV PJ
    - mlist 에 perspective와 transform-rotate를 줘서 누워 있는 느낌 및 마우스 오버시 일어나게 하는 효과 주기
    - mlist li클릭시 계속 일어나게 js코드 구현
    - 극장가는 길 애니 박스 생성

---
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -0,0 +1,34 @@
+// CGV PJ 메인 페이지 JS - main.js
+
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+// 로드 구역
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("로딩완");
+
+    /***************************************************************** 
+        [ 포스터 메뉴 클릭 시 클래스 주기 ]
+         - 포스터 메뉴 클릭 시 해당 li에 클래스 "on"을 주고 나머지 li는 
+           "on"을 지워서 선택된 li만 일어서 있는 css가 적용되게 한다
+    *****************************************************************/
+
+    // 1. 대상 : .mlist 하위 li (이벤트 대상 = 변경 대상)
+    let mlist = qsa(".mlist ul>li")
+    // console.log(mlist);
+
+    // 2. 클릭 이벤트 함수 설정 for of 사용 -> 배열/컬렉션에 사용
+    for(let x of mlist) { // x는 각 요소
+          x.onclick = () => {
+            // 1. li에 클래스 초기화
+            mlist.forEach(ele=>ele.classList.remove("on"));
+            // forEach((요소, 순번, 객체)=>{코드})
+
+            // 2. 클릭된 li에 클래스 "on" 넣기 -> ClassList 사용
+            // add() 메서드 사용
+            x.classList.add("on");
+          } // 클릭이벤트함수
+        } // for of문
+
+
+}); //// 로드 구역
