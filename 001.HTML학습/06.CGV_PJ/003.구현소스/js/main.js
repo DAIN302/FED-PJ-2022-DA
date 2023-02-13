cb153624175871290d45ab56565b0f574ac1bcc1
DAIN302 <dbgnfk12@gmail.com>
2023-02-13 15:23:05 +0900

    CGV PJ 구글맵넣기, sns링크 연결
    - 구글맵 열기 및 닫기 JS코드 작성
    - sns링크 연결

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -36,6 +36,60 @@ window.addEventListener("DOMContentLoaded", () => {
           - 구글맵박스의 닫기버튼 클릭 시 구글맵 퇴장
           -> 방법 : .gmap에 "on" 클래스 추가/제거
     **********************************************************************/     
+    // 1. 대상선정
+    // 1-1. 이벤트 대상 : .anibx -> 애니메이션 버튼 박스
+    const anibx = qs(".anibx");
+    // 1-2. 이벤트 대상 : .cbtn -> 닫기 버튼
+    const cbtn = qs(".cbtn");
+    // 1-3. 변경 대상 : .gmap -> 구글맵박스
+    const gmap = qs(".gmap");
+    
+    // console.log(anibx, gmap);
+    
+    // 2. 클릭이벤트 설정 
+    // -> 클릭 시 anibx 및 구글맵박스에 "on" 클래스 추가
+    anibx.onclick = () => {
+        anibx.classList.add("on");
+        gmap.classList.add("on");
+    };
 
+    // 3. 닫기 버튼 클릭 시 anibx 및 구글맵박스에 "on" 클래스 제거    
+    cbtn.onclick = () => {
+        anibx.classList.remove("on");
+        gmap.classList.remove("on");
+    }
+
+
+    /*
+        [ SNS 버튼 링크 연결 ]
+    */
+   // 1. 대상선정 : .sns a
+   const sns = qsa(".sns a");
+   // 2. 클릭이벤트 설정
+   // forEach((요소, 순번, 객체)=>{코드})
+   sns.forEach((ele) => {
+    ele.onclick = () => {
+        // 1. 내부텍스트 읽어오기
+        let btxt = ele.innerText;
+        // console.log(btxt);
+
+        // 이동주소 변수
+        let url="";
+
+        // 2. 분기문 url 할당
+        switch(btxt) {
+            case "페이스북 바로가기": url="https://www.facebook.com/CJCGV";break;
+            case "트위터 바로가기": url="https://twitter.com/CGV_ID";break;
+            case "인스타그램 바로가기": url="https://www.instagram.com/cgv_korea/";break;
+        } // switch case
+
+        // 3. 페이지 이동하기
+        // 새창 -> window.open()
+        // 페이지 이동 -> location.href = url값
+        window.open().location.href = url;
+
+    };
+   });//////////// forEach /////////////
+    
 
 }); //// 로드 구역
