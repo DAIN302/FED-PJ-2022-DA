f96a98f3781ea0b73ab46ca829541d894315ac8b
DAIN302 <dbgnfk12@gmail.com>
2023-03-14 10:21:21 +0900

    Update main.js

--- 001.HTML학습/10.Ossomae_PJ/js/main.js
+++ 001.HTML학습/10.Ossomae_PJ/js/main.js
@@ -5,6 +5,31 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     console.log("로딩완료!");
 
-    
+    // 대상 : .gbx
+    const gbx = document.querySelector(".gbx");
+
+
+    // 오른쪽 버튼 클릭
+    document.querySelector(".rb").onclick = () =>{
+        // 이동대상 : .gbx>div
+        let tg = gbx.querySelectorAll("div");
+
+        // 첫번째 자식 요소 div 맨 뒤로 이동
+        // appendChild(첫번째요소)
+        gbx.appendChild(tg[0]);
+        
+    } // 오른쪽 버튼 클릭
+
+    // 왼쪽 버튼 클릭
+    document.querySelector(".lb").onclick = () =>{
+        // 이동대상 : .gbx>div
+        let tg = gbx.querySelectorAll("div");
+
+        // 마지막 자식 요소 div 맨앞 이동
+        // insertBefore(마지막요소, 첫번째요소)
+        gbx.insertBefore(tg[tg.length-1], tg[0]);
+        
+    } // 왼쪽 버튼 클릭
+
 }); ///////////// 로딩구역 //////////////////////
 /////////////////////////////////////////////////
