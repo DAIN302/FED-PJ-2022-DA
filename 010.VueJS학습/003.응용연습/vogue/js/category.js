44403a727d76013a2e6d9561ac2cd8f6a863f15b
DAIN302 <dbgnfk12@gmail.com>
2023-04-20 16:18:34 +0900

    Vue JS

--- 010.VueJS학습/003.응용연습/vogue/js/category.js
+++ 010.VueJS학습/003.응용연습/vogue/js/category.js
@@ -17,7 +17,7 @@ pm = decodeURIComponent(pm);
 
 $(()=>{ ///// JQB
     // Vue JS 데이터 바인딩 코드
-    new Vue({
+    const vmCont = new Vue({
         // 대상선정 : 메인컨텐츠 영역
         el : "#cont",
         data : {
@@ -28,8 +28,37 @@ $(()=>{ ///// JQB
         mounted : function(){
             // 엑시오스로 제이슨 연결하여 데이터 가져오기
             axios.get("./js/mdata.json").then(x=>this.items=x);
-        }
 
+            // 탭메뉴 타이틀 변경
+            $("title").prepend(pm+" ");
+        }
     }); ///// Vue ///////////////
+
+    // GNB 메뉴 SPA를 위한 재정의
+    // -> 뷰JS 인스턴스를 생성하면 대상요소가 기존 JS의 모든 기능 초기화하여 작동 X
+    new Vue({
+        el : "#gnb",
+        data : {
+
+        },
+        methods : {
+            // 메뉴변경
+            chgMenu(e){
+                console.log("ㅎㅇ");
+                // 컨텐츠 영역의 뷰에 설정된 변수 catName에 접근하여 변수 값 변경
+                // vmCont에 인스턴스가 담겼으므로 
+                // vmCont.catName 로 접근하여 클릭된 요소의 글자를 읽고 이를 소문자로 변경
+                // " & " 를 "-"로 치환만 해주면 됨
+
+                // a요소 문자열 변경
+                let txt = e.target.innerText.replace(" & ", "-").toLowerCase();
+
+                // 뷰데이터에 반영 -> 가상돔의 변경 -> 실제 돔반영
+                vmCont.catName = txt
+                console.log(txt);
+                $("title").text(vmCont.catName+" | 2023 보그 코리아 (Vogue Korea)")
+            }
+        }, ////// methods
+    }) /////// GNB Vue
 
 }) ////// JQB
