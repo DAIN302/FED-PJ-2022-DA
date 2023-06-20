c63ee5ebdc01c638d1264cafd29139f66a59f78f
DAIN302 <dbgnfk12@gmail.com>
2023-06-20 14:29:42 +0900

    리액트

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
@@ -2,6 +2,9 @@
 import { useLocation } from "react-router-dom"
 // 라우터 파라미터값 받아서 데이터 처리
 
+import "./css/detail.css"
+import Ban from "./modules/Ban";
+
 function Detail(props){
     // 라우터 전달값을 받기 위한 useLocation 생성 
     const loc = useLocation();
@@ -12,22 +15,43 @@ function Detail(props){
     // 2. 캐릭터설명 - "^"문자로 잘라 배열로 변경
     let cdesc = loc.state.cdesc;
     cdesc = cdesc.split("^");
-    // 3. 캐릭터상세설명
+    // 3. 캐릭터상세설명 - "^"문자로 잘라 배열로 변경
     let facts = loc.state.facts;
+    facts = facts.split("^");
 
     return(
         <>
-            <h2>{cname}</h2>
-            <div className="cdesc">
-                {
-                    cdesc.map((v,i)=>
-                        <p key={i}>{v}</p>
-                    )
-                }
-            </div>
-            <div className="facts">
-                <h3>CHARACTER FACTS</h3>
-                {facts}
+            {/* 1. 배너컴포넌트 */}
+            <Ban cat={cname}/>
+            {/* 2. 상세정보박스 */}
+            <div className="detail">
+                <div className="descbx">
+                    <h2>{cname}</h2>
+                    <div className="cdesc">
+                        {
+                            cdesc.map((v,i)=>
+                                <p key={i}>{v}</p>
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="facts">
+                    <div>
+                        <h3>CHARACTER FACTS</h3>
+                        <table>
+                            <tbody>
+                            {
+                                facts.map((v,i)=>
+                                    <tr key={i}>
+                                        <td>{v.split(":")[0]}:</td>
+                                        <td>{v.split(":")[1]}</td>
+                                    </tr>
+                                )
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </>
     )
--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Ban.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Ban.js
@@ -28,52 +28,7 @@ import $ from "jquery";
 **************************************************************************/
 
 // 로드구역 함수화
-function jqFn(){
-    $(()=>{
-        // 광클금지
-        let prot = 0;
-    
-        // 1. 버튼 클릭 시 이동기능 구현
-        $(".abtn").click(function(){
-            // 광클금지
-            if(prot) return;
-            prot = 1;
-            setTimeout(() =>prot=0, 400);
-    
-            // 1. 버튼 구분
-            let isB = $(this).is(".rb");
-    
-            // 슬라이드 타겟 설정 : 클릭된 버튼의 형제요소 슬라이더 
-            // 상대적으로 잡기 -> 슬라이드 클래스네임 가지고 있는애들 다 잡으면 안되서
-            const tg = $(this).siblings(".slider")
-            // console.log(tg)
-    
-            // 2. 분기하여 기능구현
-            // (1) 오른쪽 버튼 클릭 시 : 오른쪽에서 들어옴 (left: 0-> -100%)
-            if(isB) {
-                tg.animate({left : "-100%"}, 400, function(){
-                    // 여기서 this는 tg
-                    // 첫번째 li맨뒤로 보내기
-                    $(this).append($(this).find("li").first()).css({left : "0"})
-                })
-            } // if
-            // (2) 왼쪽 버튼 클릭 시 : 왼쪽에서 들어옴 (left : -100%->0)
-            else {
-                // 마지막 li 맨앞이동 + 동시에 left : -100% 후 left :0 애니
-                tg.prepend(tg.find("li").last()).css({left : "-100%"})
-                .animate({left : "0"}, 400);
-            } // else
-    
-            // 3. 배너와 일치하는 블릿에 클래스 "on" 넣기(나머지는 "on" 제거)
-            // 대상 : .indic li
-            // eq(순번) -> 오른쪽이동 1, 왼쪽이동시 0
-            // isB 값으로 오른쪽은 true->1, 왼쪽은 false->0
-            // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq 값을 읽어옴
-            $(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on")
-            .siblings().removeClass("on")
-        }) // abtn click
-    }); /////////// JQB
-} /// jqFn
+// function jqFn(){} /// jqFn
 
 // 반복리스트 코드 생성용 컴포넌트
 function MakeList(props){ // rec - 개별레코드값(객체형식)
@@ -81,10 +36,22 @@ function MakeList(props){ // rec - 개별레코드값(객체형식)
         <li data-seq={props.idx}>
             <img className="banimg" src={props.rec["src"]} alt="배너"/>
             <section className="bantit">
-                <h3>{props.rec["tit1"]}</h3>
-                <h2>{props.rec["tit2"]}</h2>
-                <p>{props.rec["cont"]}</p>
-                <button>{props.rec["btn"]}</button>
+                {
+                    props.rec["tit1"] !== "" &&
+                    <h3>{props.rec["tit1"]}</h3>
+                }
+                {
+                    props.rec["tit2"] !== "" &&
+                    <h2>{props.rec["tit2"]}</h2>
+                }
+                {
+                    props.rec["cont"] !== "" &&
+                    <p>{props.rec["cont"]}</p>
+                }
+                {
+                    props.rec["btn"] !== "" &&
+                    <button>{props.rec["btn"]}</button>
+                }
             </section>
         </li>
     )
@@ -95,6 +62,52 @@ function Ban(props){
     const sel_data = ban_data[props.cat]
     // sel_data에 담긴값은 data/banner.js의 객체의 배열값
     // props.cat 배너데이터 구분 속성
+    
+    // 광클금지 변수
+    let prot = 0;
+
+    // 이동슬라이드 함수 ///
+    const goSlide = e => {
+         // 광클금지
+         if(prot) return;
+         prot = 1;
+         setTimeout(() =>prot=0, 400);
+        // 1. 이벤트가 발생한 버튼 요소
+        let ele = e.target;
+        
+        // 2. 버튼 구분
+        let isB = $(ele).is(".rb");
+    
+        // 슬라이드 타겟 설정 : 클릭된 버튼의 형제요소 슬라이더 
+        // 상대적으로 잡기 -> 슬라이드 클래스네임 가지고 있는애들 다 잡으면 안되서
+        const tg = $(ele).siblings(".slider")
+        // console.log(tg)
+
+        // 3. 분기하여 기능구현
+        // (1) 오른쪽 버튼 클릭 시 : 오른쪽에서 들어옴 (left: 0-> -100%)
+        if(isB) {
+            tg.animate({left : "-100%"}, 400, function(){
+                // 여기서 this는 tg
+                // 첫번째 li맨뒤로 보내기
+                $(this).append($(this).find("li").first()).css({left : "0"})
+            })
+        } // if
+        // (2) 왼쪽 버튼 클릭 시 : 왼쪽에서 들어옴 (left : -100%->0)
+        else {
+            // 마지막 li 맨앞이동 + 동시에 left : -100% 후 left :0 애니
+            tg.prepend(tg.find("li").last()).css({left : "-100%"})
+            .animate({left : "0"}, 400);
+        } // else
+
+        // 4. 배너와 일치하는 블릿에 클래스 "on" 넣기(나머지는 "on" 제거)
+        // 대상 : .indic li
+        // eq(순번) -> 오른쪽이동 1, 왼쪽이동시 0
+        // isB 값으로 오른쪽은 true->1, 왼쪽은 false->0
+        // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq 값을 읽어옴
+        $(ele).siblings(".indic").find("li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on")
+        .siblings().removeClass("on")
+    }; // goSlide
+
     return (
         <div className="banner">
             <ul className="slider">
@@ -109,8 +122,8 @@ function Ban(props){
                 sel_data.length > 1 &&
                 <>
                     {/* 양쪽이동버튼 */}
-                    <button className="abtn lb">＜</button>
-                    <button className="abtn rb">＞</button>
+                    <button className="abtn lb" onClick={goSlide}>＜</button>
+                    <button className="abtn rb" onClick={goSlide}>＞</button>
                     {/* 블릿 인디케이터 */}
                     <ol className="indic">
                         {
@@ -120,7 +133,7 @@ function Ban(props){
                 </>
             }
             {/* JS/jQuery 로드 구역 호출 */}
-            { jqFn() }
+            {/* { jqFn() } */}
         </div>
     )
 } // Ban 컴포넌트
